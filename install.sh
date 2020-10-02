1cfb352fce22b38b57f3e554d9df9cb2790d1741
Marcus Pauli <info@mpauli.de>
2020-10-02 17:28:37 +0000

    Update install.sh
    Deleting the tor sources list prio of writing to it
    Changed the tor sources to the values recomendet by the tor Wiki

--- install.sh
+++ install.sh
@@ -9,10 +9,18 @@
 #
 # https://github.com/phage-nz/ph0neutria
 
-echo 'deb http://deb.torproject.org/torproject.org xenial main' >> /etc/apt/sources.list.d/tor.list
-echo 'deb-src http://deb.torproject.org/torproject.org xenial main' >> /etc/apt/sources.list.d/tor.list
-gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
-gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+## echo 'deb http://deb.torproject.org/torproject.org xenial main' >> /etc/apt/sources.list.d/tor.list
+## echo 'deb-src http://deb.torproject.org/torproject.org xenial main' >> /etc/apt/sources.list.d/tor.list
+## gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
+## gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+## MP
+## decided to delete any existing tor.source list
+rm /etc/apt/sources.list.d/tor.list
+## as per the tor-Wikipage, this has changed to:
+echo 'deb https://deb.torproject.org/torproject.org bionic main' > /etc/apt/sources.list.d/tor.list
+echo 'deb-src https://deb.torproject.org/torproject.org bionic main' >> /etc/apt/sources.list.d/tor.list
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 apt update && apt upgrade
 apt-get -f install autoconf automake bison build-essential clamav clamav-daemon clamav-freshclam flex gcc git libssl-dev libfuzzy-dev libffi-dev libimage-exiftool-perl libjansson-dev libmagic-dev libpcre3 libpcre3-dev libtool make p7zip-full python3-dev python3-pip ssdeep swig tor deb.torproject.org-keyring unrar -y
 pip3 install --upgrade setuptools
